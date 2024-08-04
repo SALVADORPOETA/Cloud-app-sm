6ea94b236b8e9336c40916044c65173df19bbcc0
unknown <dsml@live.com>
2024-08-04 17:22:54 -0600

    image sizes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,22 +11,44 @@ const Hero = () => {
   return (
     <div
       name="home"
-      className="w-full h-screen bg-zinc-200 flex flex-col justify-between"
+      className="w-full h-[130%] md:h-screen bg-zinc-200 flex flex-col justify-between"
     >
-      <div className="mt-24 mx-12 grid lg:mt-auto md:mx-24 md:grid-cols-2 max-w-[1240px] m-auto">
-        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+      <div className="mt-12 mx-12 grid md:mt-24 lg:mt-auto md:mx-24 md:grid-cols-2 max-w-[1240px] m-auto">
+        <div className="h-screen md:h-auto flex flex-col justify-center items-center md:items-start w-full px-2 py-8">
           <p className="text-2xl">Unique Sequencing & Production</p>
           <h1 className="py-3 text-5xl lg:text-7xl font-bold">
             Cloud Management
           </h1>
           <p className="text-2xl">This is our Tech brand.</p>
           <button className="py-3 px-6 sm:w-[60%] my-4">Get Started</button>
+          <div
+            className="relative flex flex-col md:hidden py-8  
+            mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 
+            border border-slate-300 rounded-xl text-center shadow-xl"
+          >
+            <p>Data Services</p>
+            <div className="flex justify-between flex-wrap px-4">
+              <p className="flex px-4 py-2 text-slate-500">
+                <CloudUploadIcon className="h-6 text-indigo-600" /> App Security
+              </p>
+              <p className="flex px-4 py-2 text-slate-500">
+                <DatabaseIcon className="h-6 text-indigo-600" /> Dashboard
+                Design
+              </p>
+              <p className="flex px-4 py-2 text-slate-500">
+                <ServerIcon className="h-6 text-indigo-600" /> Cloud Data
+              </p>
+              <p className="flex px-4 py-2 text-slate-500">
+                <PaperAirplaneIcon className="h-6 text-indigo-600" /> API
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          <img className="w-full" src={bgImg} alt="/" />
+        <div className="flex justify-end">
+          <img className="md:h-[70%] lg:h-[85%]" src={bgImg} alt="/" />
         </div>
         <div
-          className="absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%] 
+          className="hidden absolute md:flex flex-col py-8 md:min-w-[760px] bottom-[5%] 
             mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 
             border border-slate-300 rounded-xl text-center shadow-xl"
         >
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,35 +5,74 @@ import { Link } from 'react-scroll'
 const Navbar = () => {
   const [nav, setNav] = useState(false)
   const handleClick = () => setNav(!nav)
-  const handleClose = () => setNav(!nav)
+  const handleClose = () => setNav(false)
   return (
     <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
-          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">BRAND.</h1>
+          <h1 className="text-3xl font-bold mr-4 sm:text-4xl">
+            <Link
+              onClick={handleClose}
+              to="home"
+              smooth={true}
+              duration={500}
+              className="hover:cursor-pointer"
+            >
+              BRAND.
+            </Link>
+          </h1>
           <ul className="hidden md:flex">
-            <li className="hover:cursor-pointer">
-              <Link to="home" smooth={true} duration={500}>
+            <li>
+              <Link
+                to="home"
+                smooth={true}
+                duration={500}
+                className="hover:cursor-pointer"
+              >
                 Home
               </Link>
             </li>
-            <li className="hover:cursor-pointer">
-              <Link to="about" smooth={true} offset={-200} duration={500}>
+            <li>
+              <Link
+                to="about"
+                smooth={true}
+                offset={-200}
+                duration={500}
+                className="hover:cursor-pointer"
+              >
                 About
               </Link>
             </li>
-            <li className="hover:cursor-pointer">
-              <Link to="support" smooth={true} offset={-50} duration={500}>
+            <li>
+              <Link
+                to="support"
+                smooth={true}
+                offset={-50}
+                duration={500}
+                className="hover:cursor-pointer"
+              >
                 Support
               </Link>
             </li>
-            <li className="hover:cursor-pointer">
-              <Link to="platforms" smooth={true} offset={-100} duration={500}>
+            <li>
+              <Link
+                to="platforms"
+                smooth={true}
+                offset={-100}
+                duration={500}
+                className="hover:cursor-pointer"
+              >
                 Platforms
               </Link>
             </li>
-            <li className="hover:cursor-pointer">
-              <Link to="pricing" smooth={true} offset={-50} duration={500}>
+            <li>
+              <Link
+                to="pricing"
+                smooth={true}
+                offset={-50}
+                duration={500}
+                className="hover:cursor-pointer"
+              >
                 Pricing
               </Link>
             </li>
@@ -46,12 +85,22 @@ const Navbar = () => {
           <button className="px-4 py-2 lg:px-8 lg:py-3">Sign Up</button>
         </div>
         <div className="md:hidden mr-4" onClick={handleClick}>
-          {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
+          {!nav ? (
+            <MenuIcon className="w-5 hover:cursor-pointer" />
+          ) : (
+            <XIcon className="w-5 hover:cursor-pointer" />
+          )}
         </div>
       </div>
       <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-8'}>
         <li className="border-b-2 border-zinc-300 w-full">
-          <Link onClick={handleClose} to="home" smooth={true} duration={500}>
+          <Link
+            onClick={handleClose}
+            to="home"
+            smooth={true}
+            duration={500}
+            className="hover:cursor-pointer"
+          >
             Home
           </Link>
         </li>
@@ -62,6 +111,7 @@ const Navbar = () => {
             smooth={true}
             offset={-200}
             duration={500}
+            className="hover:cursor-pointer"
           >
             About
           </Link>
@@ -73,6 +123,7 @@ const Navbar = () => {
             smooth={true}
             offset={-50}
             duration={500}
+            className="hover:cursor-pointer"
           >
             Support
           </Link>
@@ -84,6 +135,7 @@ const Navbar = () => {
             smooth={true}
             offset={-100}
             duration={500}
+            className="hover:cursor-pointer"
           >
             Platforms
           </Link>
@@ -95,6 +147,7 @@ const Navbar = () => {
             smooth={true}
             offset={-50}
             duration={500}
+            className="hover:cursor-pointer"
           >
             Pricing
           </Link>
